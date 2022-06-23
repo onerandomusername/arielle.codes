9f7ca1092520daefc25b688fce4b977959267453
onerandomusername <genericusername414+git@gmail.com>
2022-06-23 05:29:58 -0400

    more open graph tags

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,6 +7,13 @@
 	import '$lib/themes/prism-laserwave.css';
 </script>
 
+<meta property="og:site_name" content="arielle.codes" />
+<meta property="og:title" content="arielle" />
+<meta property="og:type" content="website" />
+<meta property="og:description" content="My website." />
+<meta property="og:url" content="https://arielle.codes/" />
+<meta property="og:image" content="http://arielle.codes/favicon.png" />
+<meta name="theme-color" content="#f5a9b8" />
 <br />
 <Header />
 
